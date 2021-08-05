4eacc747d0799534d85fc7c6f042d8251c29963b
Caleb <cgoertel@gmail.com>
2021-08-05 19:17:04 -0400

    Fixed RSC glitches, visual additions, slick menus

---
+++ HyperDelivery/HyperDelivery.lua
@@ -0,0 +1,61 @@
+ModUtil.RegisterMod("HyperDelivery")
+
+-- Scripts/RoomManager.lua : 1874
+ModUtil.WrapBaseFunction("StartRoom", function ( baseFunc, ... )
+    local text_config_table = DeepCopyTable(UIData.CurrentRunDepth.TextFormat)
+
+    PrintUtil.createOverlayLine(
+        "HD5 Text",
+        "HD5 QUALIFIERS",
+        MergeTables(
+            text_config_table,
+            {
+                x_pos = 1905,
+                y_pos = 115,
+            }
+        )
+    )
+
+    baseFunc(...)
+end, HyperDelivery)
+
+-- Scripts/UIScripts.lua : 145
+ModUtil.WrapBaseFunction("ShowCombatUI", function ( baseFunc, ... )
+    local text_config_table = DeepCopyTable(UIData.CurrentRunDepth.TextFormat)
+
+    PrintUtil.createOverlayLine(
+        "HD5 Text",
+        "HD5 QUALIFIERS",
+        MergeTables(
+            text_config_table,
+            {
+                x_pos = 1905,
+                y_pos = 115,
+            }
+        )
+    )
+
+    baseFunc(...)
+end, HyperDelivery)
+
+ModUtil.WrapBaseFunction("DisplayLocationText", function( baseFunc, eventSource, args)
+    if args.Text == "Location_Tartarus" then
+        local funnytext = RandomChance(0.15)
+        args.Text = "dragonsquad esports presents"
+        if funnytext then
+            args.Text = "soft pine flooring presents"
+        end
+        args.FontScale = 0.72
+        baseFunc(eventSource, args)
+        args.Text = "HYPER DELIVERY FIVE"
+        if funnytext then
+            args.Text = "TIMBO'S SECURITY DEPOSIT"
+        end
+        args.Delay = 0.6
+        args.FontScale = 1.0
+        baseFunc(eventSource, args)
+    else
+        baseFunc(eventSource,args)
+    end
+
+end, HyperDelivery)
---
+++ QuickRestart/QuickRestart.lua
@@ -0,0 +1,91 @@
+ModUtil.RegisterMod("QuickRestart")
+
+local config = {
+  Enabled = true
+}
+QuickRestart.config = config
+
+function QuickRestart.CanReset()
+  -- QuickRestart must be Enabled
+  if not config.Enabled then return false end
+
+  -- Zag must not be in the House
+  if ModUtil.PathGet("CurrentDeathAreaRoom") then return false end
+
+  -- Zag must not be frozen
+  if not IsEmpty( CurrentRun.Hero.FreezeInputKeys ) then return false end
+
+  -- Combat UI must be visible
+  if not (ShowingCombatUI or false) then return false end
+
+  -- If we're in a Thanatos Room, enemies must have already spawned
+  if (CurrentRun ~= nil and CurrentRun.CurrentRoom ~= nil and
+      CurrentRun.CurrentRoom.Encounter ~= nil and
+      CurrentRun.CurrentRoom.Encounter.ThanatosId ~= nil
+      and GetActiveEnemyCount() == 0) then
+    return false
+  end
+
+  return true
+end
+
+function QuickRestart.ResetRun(triggerArgs)
+  if not QuickRestart.CanReset() then
+    ModUtil.Hades.PrintOverhead("Can't reset now!", 2)
+    return
+  end
+
+  if RtaTimer then
+    RtaTimer__ResetRtaTimer()
+  end
+
+  KillHero( CurrentRun.Hero, triggerArgs )
+end
+
+OnControlPressed{ "Assist",
+  function(triggerArgs)
+    if config.Enabled then
+      if IsControlDown({ Name = "Assist" })
+          and IsControlDown({ Name = "Use" })
+          and IsControlDown({ Name = "Shout" })
+          and IsControlDown({ Name = "Reload" }) then
+        QuickRestart.ResetRun(triggerArgs)
+      end
+    end
+end}
+
+OnControlPressed{ "Use",
+  function(triggerArgs)
+    if config.Enabled then
+      if IsControlDown({ Name = "Assist" })
+          and IsControlDown({ Name = "Use" })
+          and IsControlDown({ Name = "Shout" })
+          and IsControlDown({ Name = "Reload" }) then
+        QuickRestart.ResetRun(triggerArgs)
+      end
+    end
+end}
+
+OnControlPressed{ "Shout",
+  function(triggerArgs)
+    if config.Enabled then
+      if IsControlDown({ Name = "Assist" })
+          and IsControlDown({ Name = "Use" })
+          and IsControlDown({ Name = "Shout" })
+          and IsControlDown({ Name = "Reload" }) then
+        QuickRestart.ResetRun(triggerArgs)
+      end
+    end
+end}
+
+OnControlPressed{ "Reload",
+  function(triggerArgs)
+    if config.Enabled then
+      if IsControlDown({ Name = "Assist" })
+          and IsControlDown({ Name = "Use" })
+          and IsControlDown({ Name = "Shout" })
+          and IsControlDown({ Name = "Reload" }) then
+        QuickRestart.ResetRun(triggerArgs)
+      end
+    end
+end}
